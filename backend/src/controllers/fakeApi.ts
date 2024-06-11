cfced5f9a8dbcb60c6753abaa76047fff9193b29
Maxim Larkin <m.larkin@MacBook-Pro-admin-apple.local>
2024-06-11 12:34:23 +0300

    пагинация + поиск по id

--- backend/src/controllers/fakeApi.ts
+++ backend/src/controllers/fakeApi.ts
@@ -1,13 +1,46 @@
 import express from 'express';
 import { getEndpoints } from '../db/endpoints';
 
+const parseResourceId = (url: string) => {
+  const splitUrl = url.replace(/\?.+/, '').replace(/\/$/, '').split('/');
+  const parsedId = parseInt(splitUrl[splitUrl.length - 1]);
+  return isNaN(parsedId) ? null : parsedId;
+};
+const parseRoute = (url: string) => {
+  return url
+    .replace(/(\/api)/, '')
+    .replace(/\/\d+/g, '')
+    .replace(/\?.+/, '')
+    .replace(/\/$/, '');
+};
+
 export const fakeApi = async (req: express.Request, res: express.Response) => {
   try {
     const endpoints = await getEndpoints();
-    const { originalUrl, method, } = req;
-    const findEndpoint = endpoints.find((endpoint: any) => `/api${endpoint.url}` === originalUrl && endpoint.method === method);
+    const { path, method } = req;
+    const id = parseResourceId(path);
+    const route = parseRoute(path);
+
+    const findEndpoint = endpoints.find((endpoint: any) => {
+      return endpoint.url === route && endpoint.method === method
+    });
     if (findEndpoint) {
-      return res.status(findEndpoint.statusCode).json(JSON.parse(findEndpoint.mockJson));
+      const data = JSON.parse(findEndpoint.mockJson);
+      let resData;
+      if (id && Array.isArray(data)) {
+        resData = data.find(item => item._id === id);
+        if (!resData) {
+          res.status(404).json({ code: 404, message: 'Not found' });
+          return res.end();
+        }
+      } else if (Array.isArray(data)) {
+        resData = data;
+        const { limit = 20, offset = 0 } = req.query;
+        res.setHeader('Total-Count', resData.length);
+        resData.splice(0, Number(offset));
+        resData.splice(Number(limit));
+      }
+      return res.status(findEndpoint.statusCode).json(resData);
     } else {
       return res.sendStatus(404);
     }
